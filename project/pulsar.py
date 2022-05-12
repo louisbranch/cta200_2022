26f008b062a641080f95403ba7561b6052991a80
Louis Branch <louis@branch.family>
2022-05-12 10:26:29 -0400

    Fix intensity fn and add basic integration

--- project/pulsar.py
+++ project/pulsar.py
@@ -1,42 +1,48 @@
 from math import log, sin, pi
 import numpy as np
 from astropy import units as u
-from scipy.integrate import solve_ivp
+from scipy.integrate import quad
+from typing import Any
 
 def concentration(d: float) -> float:
-    """ Calculate concentration (reciprocal of dispersion) using d, the duty
+    """Calculate concentration (reciprocal of dispersion) using d, the duty
     cycle value.
     """
-    return log(2) / 2 * sin(pi * d / 2) ** 2
+    return log(2) / (2 * (sin(pi * d / 2) ** 2))
 
 def speed(period: u.s) -> u.rad/u.s:
+    """Calculate the angular speed for a given period."""
     return (2 * pi * u.rad) / period
 
-def angle(phi0: u.rad, period: u.s, time: u.s) -> u.rad:
-    return phi0 + speed(period) * time
+def angle(phi0: u.rad, period: u.s, dt: u.s) -> u.rad:
+    """Calculate the angular displacement given an initial value and angular
+    speed.
+    """
+    return phi0 + speed(period) * dt
 
-def intensity(peak: float, phi0: u.rad, d: float, period: u.s, 
-tframe: np.array) -> np.array:
+def intensity(peak: float, phi0: u.rad, d: float, period: u.s,
+ time: u.s) -> float:
 
     kappa = concentration(d)
-    
-    # For each time step, calculate the angular speed using the period.
-    # Use the angle at time t to calculate the intensity based on the 
-    # concentration.
-    def mapping(time: u.s):
-        phi = angle(phi0, period, time)
-        return peak * np.exp(kappa * np.cos(phi.value - 1))
+    phi = angle(phi0, period, time)
+    return peak * np.exp(kappa * np.cos(phi.value - 1))
+
+def linear_intensity(peak: float, phi0: u.rad, d: float, period: u.s, 
+tframe: np.array) -> np.array:
+
+    mapping = lambda t: intensity(peak, phi0, d, period, t)
 
     return np.apply_along_axis(mapping, 0, tframe)
 
-def integrated_intensity(peak: float, phi0: u.rad, d:float, period: u.s, 
-interval: u.s, k: float):
+def integrated_intensity(peak: float, phi0: u.rad, d: float, period: u.s,
+ts: u.s, steps: np.array):
 
-    y0 = (0,)
-    tspan = (k, k + 1)
-    args = (1,)
+    integrand = lambda t : intensity(peak, phi0, d, period, t*ts)
 
-    def integration(time: u.s):
-        return None
+    k_integrated = []
 
-    return solve_ivp(integration, tspan, y0, args=args, dense_output=True)
+    for k in steps:
+        result, err = quad(integrand, k, k+1)
+        k_integrated.append(result / ts.value)
+
+    return k_integrated
